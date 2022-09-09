171f81adcb96c2000720fc9c62c3648a4db90c04
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-09 05:20:29 -0700

    Task 8. Advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this progam prints "and that piece of art is useful - Dora Korpar, 2015-10-19,"
+ * return: 0
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	return (1);
+
+}
