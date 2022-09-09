51c62af3678570bec9d1e27010d4c0480cbc8642
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-09 03:15:49 -0700

    Programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - The beginning_printing. Entry point
+ *
+ *Return: this must always be 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
