f0d72c6c562cdd237fac47864f8e34bce4a3f17e
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-09 04:25:09 -0700

    Task 5, again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /**
  * Main - Printing. entry point
  *
+ * Description: useing the main funtion
+ * this prints "with proer grammar, but the outcome is a piece of art,"
  * Return - Always zero 0
 */
 int main(void)
 {
-	printf(" with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
