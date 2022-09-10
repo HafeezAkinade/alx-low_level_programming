dc4b9c3a2b5f5da87715456ec6b23b8c3b7009d1
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-09 19:51:52 -0700

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print random number to variable n
+ *
+ * Description: using the amin function
+ * this program assign random numbers to the variable n.
+ * Return: 0
+*/
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
