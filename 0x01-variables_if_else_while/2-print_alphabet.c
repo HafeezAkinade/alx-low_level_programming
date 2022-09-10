eab47cc85ac76ed6af3f096a5274944eb555cbf1
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-09 20:31:39 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print and assign random number to n
+ *
+ * Description: using main function
+ * this program assign ramdon numbers to variable n
+ * Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(cha);
+	}
+	putchar('\n');
+	
+	return (0);
+}
