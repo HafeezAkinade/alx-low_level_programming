476072b07d4b0b354d23c15a2439806a60c7ae3d
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-10 00:41:01 -0700

    9 again

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print numbers from 0-9 with commas and spance in between
+ * main - print numbers from 0-9 with commas and space in between
  *
  * Description: using main function 
  * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
