a07f780d2d6ea0ceb7d610a64a6e7fb03ff4480c
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-09 21:07:34 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: usig the main function
+ * this programs assign
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
