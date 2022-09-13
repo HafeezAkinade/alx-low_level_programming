b2d9a8c08e464005b9fef594982145754ecaecac
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-13 06:43:58 -0700

    task 1.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _putchar - prints a to z
+ *
+ * Return: always 0
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z', c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
