6b404777f8c0832980aeb4c998314cdc49ad5002
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-13 06:28:48 -0700

    Task 0.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include "_putchar.h"
+#include "main.h"
 /**
- * main - Entry point
+ * main - print _putchar
  *
- *Return: 0
+ *Return: always 0
 */
 int main(void)
 {
-	char _putchar(9) = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-	unsigned int c;
-
-	for (c = 0; c < sizeof(_putchar); c++)
-	{
-		_putchar(_putchar[c]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
