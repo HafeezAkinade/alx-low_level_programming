4aaa75d479e86bbdc8d7cb6a8fea9fa5926e6353
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-15 01:04:11 -0700

    Task 0. Debugging.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - this tests function prints if integer is positive or negative
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	1 = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging in C duck debugging
