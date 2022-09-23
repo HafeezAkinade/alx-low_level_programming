08186b18ae4e3005fe922ac686ba8d140b43eec1
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-23 01:11:00 -0700

    Task 9.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,6 @@
 void print_number(int n)
 {
 	unsigned int n1;
-
 	n1 = n;
 
 	if (n < 0)
@@ -17,7 +16,7 @@ void print_number(int n)
 		_putchar('-');
 		n1 = -n;
 	}
-	if (n1 / 10 != 0)
+	else if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
