002f45e9fd70dc71ca3b0b4c7fe528b2593e4ba9
HafeezAkinade <hafeez.akinade@gmail.com>
2022-09-24 10:30:01 +0100

    Corrections

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,19 +6,32 @@
  *
  * Return: always 0.
 */
+
 void print_number(int n)
 {
-	unsigned int n1;
-	n1 = n;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+		_putchar(45);
+		m = n * -1;
 	}
-	else if (n1 / 10 != 0)
+	else
 	{
-		print_number(n1 / 10);
+		m = n;
 	}
-	_putchar((n1 % 10) + '0');
+
+	d = m;
+	count = 1;
+
+	while (d> 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
