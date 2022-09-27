8cee2996d93bbf017733973fd7c84cca7ed45771
hafeezakinade <hafeez.akinade@gmail.com>
2022-09-27 03:12:44 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,8 +4,8 @@
 /**
  * void prototypes()
  * int prototypes()
- * char prototypes()
 */
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -15,6 +15,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int _putchar(char);
 
 #endif /*MAIN_H*/
